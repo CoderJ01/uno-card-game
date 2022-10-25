ae61c7333fff3da98340a443eab8851d806d5399
Joshua <buildingguy@sbcglobal.net>
2022-10-24 21:47:17 -0700

    create method to remove card from bottom of the stack

--- Deck.java
+++ Deck.java
@@ -26,4 +26,12 @@ public class Deck {
         this.deck.remove(topCard);
         return topCard;
     }
+
+    // remove the bottom card to start the game
+    // this will be the first card in the discard pile
+    public Card firstDiscard() {
+        Card bottomCard = deck.get(0);
+        this.deck.remove(bottomCard);
+        return bottomCard;
+    }
 }
