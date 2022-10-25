c71b7dba7a18d99bbc221a49e39ab0aed42a1516
Joshua <buildingguy@sbcglobal.net>
2022-10-24 20:40:23 -0700

    test card objects

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // test wild card
+        Card wild = new Card("wild");
+        System.out.println(wild.getCardName());
+        System.out.println(wild.getCardPoints());
+
+        // test numbered card
+        Card blueEight = new Card("B8", "blue", "8");
+        System.out.println(blueEight.getCardName());
+        System.out.println(blueEight.getCardColor());
+        System.out.println(blueEight.getCardSymbol());
+        System.out.println(blueEight.getCardPoints());
+
+        // test special card
+        Card skip = new Card("skip (green)", "green", "skip");
+        System.out.println(skip.getCardName());
+        System.out.println(skip.getCardColor());
+        System.out.println(skip.getCardSymbol());
+        System.out.println(skip.getCardPoints());
     }
 }
