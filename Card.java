fc120270a52ca88a9a8434330f8ec979c1b54b54
Joshua <buildingguy@sbcglobal.net>
2022-10-26 15:48:54 -0700

    make 0 card worth 10 points:

--- Card.java
+++ Card.java
@@ -19,9 +19,12 @@ public class Card {
         this.symbol = symbol;
         if(symbol.equals("1") || symbol.equals("2") || symbol.equals("3") || symbol.equals("4") || 
            symbol.equals("5") || symbol.equals("6") || symbol.equals("7") || symbol.equals("8") ||
-           symbol.equals("9") || symbol.equals("0")) 
+           symbol.equals("9")) 
         {
             this.points = Integer.parseInt(symbol);
+        }
+        else if(symbol.equals("0")) {
+            this.points = 10;
         }
         else {
             this.points = 20;
