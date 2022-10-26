1d2276467be74a61166415d713b5776c42511256
Joshua <buildingguy@sbcglobal.net>
2022-10-25 18:25:23 -0700

    display players card modification (@Override)

--- PlayerHand.java
+++ PlayerHand.java
@@ -1,4 +1,14 @@
 public class PlayerHand extends Deck {
     // constructor
     public PlayerHand () {}
+
+    // display cards in deck
+    @Override
+    public void displayCards() {
+        System.out.println("Your Hand");
+        System.out.println("*********");
+        for(Card card : returnCards()) {
+            System.out.println(card.getCardName() + " ");
+        }
+    }
 }
