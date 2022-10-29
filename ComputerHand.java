a37ccf790aa76bca45048239e92bb089861b5b4f
Joshua <buildingguy@sbcglobal.net>
2022-10-28 18:51:31 -0700

    display the card that the CPU discards

--- ComputerHand.java
+++ ComputerHand.java
@@ -56,6 +56,7 @@ public class ComputerHand extends PlayerHand {
             }
         }
         delay();
+        System.out.println(cardToReturn.getCardName());
         return cardToReturn;
     }
 
