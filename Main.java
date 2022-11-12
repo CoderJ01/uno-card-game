8f5de38c2d66e2a61c8bb6e392e4d609a5e31283
Joshua <buildingguy@sbcglobal.net>
2022-11-12 13:25:19 -0800

    edit

--- Main.java
+++ Main.java
@@ -353,7 +353,7 @@ public class Main {
         System.out.println("\nB = blue, G = green, R = red, and Y = yellow; W = wild, W+4 = wild +4");
         System.out.println("\nThe discard pile shows only its top card. Unless any wild type card is placed, then players must select a card");
         System.out.println("that matches either the color or number of the top card.");
-        System.out.println("\nThe player that gets to play first will be chosen randomly");
+        System.out.println("\nThe player that gets to play first will be chosen randomly.");
 
         // confirm that player understands the rules
         String confirmation = "";
