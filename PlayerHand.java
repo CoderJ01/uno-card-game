8990bf0eadd4c11375ec2f7e97a6a13cb515cca5
Joshua <buildingguy@sbcglobal.net>
2022-11-14 13:28:06 -0800

    notify player to pass hand

--- PlayerHand.java
+++ PlayerHand.java
@@ -132,6 +132,19 @@ public class PlayerHand extends Deck implements Comparable<PlayerHand>{
 
     // pass deck to the next player 
     public void passDeck(List<Card> newDeck) {
+        promptToPass();
         this.playerDeck = new ArrayList<>(newDeck);
+    }
+
+    // notify player to pass hand
+    private void promptToPass() {
+        String pass = "";
+        while(true) {
+            System.out.print("You must pass your hand to the next player. Enter 'p' to pass hand: ");
+            pass = input.next();
+            if(pass.equals("p")) {
+                break;
+            }
+        }
     }
 }
