7e593e46626c4371e37c9ef7c7e477701e6f2a73
Joshua <buildingguy@sbcglobal.net>
2022-11-14 13:36:54 -0800

    edit prompt wording

--- PlayerHand.java
+++ PlayerHand.java
@@ -140,7 +140,7 @@ public class PlayerHand extends Deck implements Comparable<PlayerHand>{
     private void promptToPass() {
         String pass = "";
         while(true) {
-            System.out.print("\nYou must pass your hand to the next player. Enter 'p' to pass hand: ");
+            System.out.print("\nAll players must pass their respective hands to the next player. Enter 'p' to pass your hand: ");
             pass = input.next();
             if(pass.equals("p")) {
                 break;
