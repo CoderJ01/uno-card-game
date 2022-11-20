8471726a9b29abaa00abc75a06fd657ad8e88e33
Joshua <buildingguy@sbcglobal.net>
2022-11-20 13:28:55 -0800

    display points more neatly

--- Main.java
+++ Main.java
@@ -234,12 +234,10 @@ public final class Main {
    
     // display the amount of points for each player 
     private static void displayPoints() { 
-        int i = 1;
         System.out.println("\nPOINTS");
         System.out.println("******");
         for(PlayerHand player : players) {
-            System.out.println("Player " + i + " (" + player.getPlayerName() + ") had " + player.tallyPoints() + " points remaining");
-            i++;
+            System.out.println(player.getPlayerName() + " had " + player.tallyPoints() + " points remaining");
         }
     }
 
