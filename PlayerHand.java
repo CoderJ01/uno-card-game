f392656b2ac7cb7a22908a7fceda1194df432ae8
Joshua <buildingguy@sbcglobal.net>
2022-11-28 20:18:51 -0800

    edit

--- PlayerHand.java
+++ PlayerHand.java
@@ -63,7 +63,7 @@ public class PlayerHand extends Deck implements Comparable<PlayerHand>{
     }
 
     // ask if player would like to place a card onto the discard pile
-    public boolean pickOrNo(Card card) {
+    public boolean pickOrNo(Card placeholder) {
         String placeCard = "";
         // allow player to select only 'y' for "yes" or 'n' for "no"
         while(true) {
